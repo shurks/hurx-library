474ee0aa6515f9c6750e7c6d779ed404d48878f0
Stan Hurks <stan@hurks.digital>
2023-04-10 20:45:27 +0200

    Fixed a bug where prepackage would be called immediately

--- src/prepackage.ts
+++ src/prepackage.ts
@@ -4,7 +4,7 @@ import path from 'path'
 // DO NOT DELETE THIS FILE
 // This file is used by build system to build a clean npm package with the compiled js files in the root of the package.
 // It will not be included in the npm package.
-(() => {
+const prepackage = (() => {
     const source = fs.readFileSync(path.resolve('./package.json')).toString('utf-8')
     const sourceObj = JSON.parse(source)
     sourceObj.scripts = {}
@@ -20,4 +20,7 @@ import path from 'path'
     }
     fs.writeFileSync(path.resolve('./dist', 'package.json'), Buffer.from(JSON.stringify(sourceObj, null, 4), "utf-8"))
     fs.writeFileSync(path.resolve('./dist', 'version.txt'), Buffer.from(sourceObj.version, "utf-8"))
-})()
+})
+if (process.argv.includes('--execute-prepackage')) {
+    prepackage()
+}
